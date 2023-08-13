66a67315398c66f9a469dcc64af989367f19c013
hanane20032003 <h1a9n7ane@gmail.com>
2023-08-13 11:26:21 +0330

    phase6

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,16 @@
 
 from django.conf import settings
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
-from jdatetime import datetime as dt
 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=dt.now)
-    published_date = models.DateTimeField(default=dt.now,blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(default=timezone.now,blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
